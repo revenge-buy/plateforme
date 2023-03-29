cdb7cab2f83f09e2425045ba511c26a7bff0d7e0
Durin TASONDOCK <temgoua484@gmail.com>
2023-03-29 10:57:28 +0100

    trying deployment wit vercel

--- next.config.js
+++ next.config.js
@@ -1,24 +1,24 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+// const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ''
-let basePath = '/'
+// let assetPrefix = ''
+// let basePath = '/'
 
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
+// if (isGithubActions) {
+//   // trim off `<owner>/`
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+//   assetPrefix = `/${repo}/`
+//   basePath = `/${repo}`
+// }
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  // assetPrefix: assetPrefix,
+  // basePath: basePath,
   images: {
-    loader: 'imgix',
-    path: 'the "domain" of your Imigix source',
+    // loader: 'imgix',
+    // path: 'the "domain" of your Imigix source',
     remotePatterns: [
       {
         protocol: 'https',
