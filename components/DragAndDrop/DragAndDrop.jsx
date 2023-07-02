7002d4bfc9c489588ab8af86062ae67f3913f906
Durin TASONDOCK <temgoua484@gmail.com>
2023-07-02 04:14:11 +0100

    commit

--- components/DragAndDrop/DragAndDrop.jsx
+++ components/DragAndDrop/DragAndDrop.jsx
@@ -12,12 +12,12 @@ const DragAndDrop = ({ file, setFile, title, text, editing }) => {
   const [fileDataUrl, setFileDataUrl] = useState(null)
   const [initialRemoteFile, setInitialRemoteFile] = useState(null)
 
-  function setPicture(e){
+  function setPicture(e) {
     const _file = e.target.files[0];
     setFile(_file);
   }
 
-  function resetFile(){
+  function resetFile() {
     setFile(initialRemoteFile)
   }
 
@@ -42,29 +42,29 @@ const DragAndDrop = ({ file, setFile, title, text, editing }) => {
       }
     }
   }, [file])
-  
+
 
   return (
     <div className={`input-set ${styles.dragBox}`}>
-      <label>{ title }</label>
+      <label>{title}</label>
       {!file
         ? <div className={`${styles.dragBoxContent}`}>
-            <input type="file" accept='image/*' name="cover" multiple={false} onChange={setPicture} />
-            <span className={`${styles.dragButton}`}>
-              Importez une image
-              <BiImport />
-            </span>
-            <Image 
-              src="/product.jpg" 
-              alt="preview" 
-              width={300} 
-              height={300}
-            />
-          </div>
+          <input type="file" accept='image/*' name="cover" multiple={false} onChange={setPicture} />
+          <span className={`${styles.dragButton}`}>
+            Importez une image
+            <BiImport />
+          </span>
+          <Image
+            src="/product.jpg"
+            alt="preview"
+            width={300}
+            height={300}
+          />
+        </div>
         : <div className={`${styles.dragBoxContent}`}>
           {
             (!fileDataUrl && !file.remote)
-            ? <div>
+              ? <div>
                 <h4>Attention</h4>
                 <p>Une erreur s&apos;est produite lors du chargement de votre image</p>
                 <span className={`${styles.dragButton}`}>
@@ -72,20 +72,20 @@ const DragAndDrop = ({ file, setFile, title, text, editing }) => {
                   <p>Recommencer !</p>
                 </span>
               </div>
-            : <div>
-                <Image src={file.remote ? file?.url ? file.url: "/product.jpg" : fileDataUrl} alt="preview" width={200} height={200} />
+              : <div>
+                <Image src={file.remote ? file?.url ? file.url : "/product.jpg" : fileDataUrl} alt="preview" width={200} height={200} />
                 <div>
                   <span className={`${styles.dragButton2}`}>
                     <input type="file" accept='image/*' name="cover" multiple={false} onChange={setPicture} />
                     <BsPencil />
                   </span>
                 </div>
-            </div>
+              </div>
           }
         </div>
       }
-      {(!file?.url && editing) && <Reload label="Reinitialiser l'image" func={resetFile} /> }
-      <p>{ text }</p>
+      {(!file?.url && editing) && <Reload label="Reinitialiser l'image" func={resetFile} />}
+      <p>{text}</p>
     </div>
   )
 }
